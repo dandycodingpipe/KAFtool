7e59bbcace1867ec010ace25e7e7f6f5f2ebe0ac
Chris <christian.a.hernandezf@hotmail.com>
2023-06-21 16:49:03 +0200

    XML processing function is relatively complete

---
+++ R/AOP_XML_processor.R
@@ -0,0 +1,82 @@
+# AOP-Wiki quarterly backup processing script
+#' Converts AOP-wiki XML files into R data frames for further processing
+#'
+#' This function takes XML and converts
+#' @name AOP_XML_children_organizer
+#' @param doc XML file downloaded from AOP-wiki
+#' @keywords Classification, AOP-wiki, AOP
+#' @export
+#' @examples
+#' Word_Cleaner(data = association_rules_df)
+
+  library(XML)
+
+  #testing
+  doc <- "aop-wiki-xml-2023-04-01(1)"
+
+AOP_XML_children_organizer <- function(doc) {
+
+  doc <- xmlRoot(xmlTreeParse(doc))
+
+  class_frequencies <- table(names(doc))
+
+  class_coordinates <- names(names(doc))
+
+  unique_classes <- unique(class_coordinates)
+
+  # Create a list containing the separated coordinates for every instance of every class
+
+  my_list <- vector("list", length(unique_classes))
+  names(my_list) <- unique_classes
+
+    for(i in 1:length(unique_classes)){
+
+      class_iteration_array <- which(class_coordinates == unique_classes[i])
+      my_list[[i]] <- class_iteration_array
+
+    }
+
+tmp = xmlSApply(doc, function(x) xmlSApply(x, xmlValue))
+
+
+tester <- data.frame(AOP_Class = 0, value = 0)
+
+for(h in 1:length(unique_classes)){
+
+  label <- unique_classes[h]
+
+  for(i in 1:length(my_list[[label]])){
+    print(label)
+    # Use switch case based on the value of the number variable
+    result <- switch(label,
+                     "chemical" = "preferred-name",
+                     "biological-object" = "name",
+                     "biological-process" = "name",
+                     "biological-action" = "name",
+                     "stressor" = "name",
+                     "taxonomy" = "name",
+                     "key-event" = "short-name",
+                     "aop" = "short-name",
+                     default = NULL)
+
+    print(result)
+    coordinate <- my_list[[label]][i]
+    print(coordinate)
+
+    if (!is.null(result)) {
+      text <- tmp[[coordinate]][result]
+      print(text)
+      new_row <- c(label, toString(text))
+      tester <- rbind(tester, new_row)
+    }
+
+  }
+}
+return(tester)
+}
+
+
+
+
+
+
