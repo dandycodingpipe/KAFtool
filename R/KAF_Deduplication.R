2d54f66f166029986be636be322f1e320720abc3
Chris <christian.a.hernandezf@hotmail.com>
2023-06-28 17:33:48 +0200

    Deduplication script

--- R/KAF_Deduplication.R
+++ R/KAF_Deduplication.R
@@ -11,38 +11,68 @@
 
 KAF_Deduplication <- function(rules) {
 
+  # Simplify data frame to basic elements we need (8 columns to 2 columns)
+  broken_down <- Rule_Concatenator(rules)
+
+  # Locality Sensitive Hashing Package
   library(textreuse)
 
-  #Concatenate and filter rules according to their RHS
-  broken_down_rules <- Rule_Concatenator(rules)
-  rules_to_remove <- c()
+  #Defining hash table parameters
+  minhash <- minhash_generator(n = 1000, seed = 3552)
 
-  RHS_len <-  length(unique(broken_down_rules$rules.RHS))
+  #Structures string documents for LSH (change the 1 to any number to compare different "pre-bucket" sets of data)
+  corpus <- TextReuseCorpus(text = broken_down$toFuzzy, tokenizer = tokenize_ngrams, n = 1,
+                            minhash_func = minhash, keep_tokens = TRUE,
+                            progress = TRUE)
+
+  #Personal calculators for determining the probability an item will be put in a bucket or that a match will be made(?)
+  lsh_threshold(h = 5000, b = 200)
+
+  #what is this?
+  lsh_probability(h = 5000, b = 200, s = 0.0)
 
 
-   for(i in 1:RHS_len){
+  #Create buckets for storing potentially similar keys
+  buckets <- lsh(corpus, bands = 100, progress = TRUE)
 
-     #Defining hash table parameters
-     minhash <- minhash_generator(n = 100, seed = 3552)
-     #Convert the current RHS rules into keys
-     corpus <- TextReuseCorpus(text = broken_down_rules$toFuzzy[which(broken_down$rules.RHS == broken_down$rules.RHS[i])], tokenizer = tokenize_ngrams, n = 1,
-                               minhash_func = minhash, keep_tokens = TRUE,
-                               progress = TRUE)
-     #Generate buckets signifying potential matches
-     buckets <- lsh(corpus, bands = 25, progress = TRUE)
-     #Display results
-     final <- lsh_compare(lsh_candidates(buckets), corpus, jaccard_similarity, progress = TRUE)
-     final = final[which(final$score > 0.75),]
+  #Calculate and extract scores according to my threshold
+  final <- lsh_compare(lsh_candidates(buckets), corpus, jaccard_similarity, progress = TRUE)
+  final = final[which(final$score >= 0.9),]
 
-     #NOTE TO SELF: THESE PARAMETERS MUST BE COMPARED FOR PERFORMANCE
+  #The workflow of removing duplicates
+  toKeep <- c()
+  toRemove <- c()
+  final$a = substr(final$a, 5, nchar(final$a))
+  final$b = substr(final$b, 5, nchar(final$b))
+  LSH_doc_i <- unique(final$a)
 
-     #Recursive process
 
-     #1. Create an empty array to hold coordinates to remove, and one for positive coordinates to keep
-     #2. For every unique doc-n that is not already in the removal array, add it to the positive coordinate array, and return the documents stored in it's column B
-     #3. While this item in column B is not in the positive array and not in the remove array, add it to the removal array
+  for(k in 1:length(LSH_doc_i)){
+
+    if(LSH_doc_i[k]%in%toRemove){
+      LSH_doc_i_coord <- final$b[which(final$a == LSH_doc_i[k])]
+      toRemove <- c(toRemove, LSH_doc_i_coord)
+
+    } else {
+
+      toKeep <- c(toKeep,LSH_doc_i[k])
+
+      LSH_doc_i_coord <- which(final$a == LSH_doc_i[k])
+
+      for(i in 1:length(LSH_doc_i_coord)){
+        if(!(final$b[LSH_doc_i_coord[i]] %in% toKeep)){
+          toRemove <- c(toRemove,final$b[LSH_doc_i_coord[i]])
+        }
+      }
 
     }
 
-  return(clean_rules)
+  }
+
+  toKeep <- unique(toKeep)
+  toRemove <- unique(toRemove)
+  print(paste(length(toRemove)))
+  broken_down <- broken_down[-c(as.numeric(toRemove)),]
+
+  return(broken_down)
 }
